b9b719767932d84b5bf62551f69499804e452d8d
HermesWu <1136025519@qq.com>
2018-04-24 11:03:10 +0800

    画笔粗细

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ function drawCirle (x, y, radius) {
 }
 function drawLine (x1, y1, x2, y2) {
 	context.beginPath()
-	context.lineWidth = 5
+	context.lineWidth = lineWidth
 	context.moveTo(x1, y1)
 	context.lineTo(x2, y2)
 	context.stroke()
